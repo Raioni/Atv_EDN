4d092270b3e45dd1cc3089d013eba270fdbb3966
Raioni D <87760199+Raioni@users.noreply.github.com>
2025-07-22 21:22:17 -0300

    Add files via upload

---
+++ q4.py
@@ -0,0 +1,8 @@
+nome = "Cadeira Infantil"
+uni = round(12.40, 2)
+qtd = 3
+
+print("Nome do Produto:", nome)
+print(f"Preço Unitario: R$ {uni:.2f}")
+print("Quantidade:", qtd)
+print(f"\nPREÇO TOTAL: R$ {uni * qtd:.2f}")
